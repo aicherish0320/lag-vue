079a19bec7c1acadc8abab199125309fd6d0a91c
aicherish <aicherish0320@163.com>
2022-03-09 22:20:12 +0800

    feat: 创建 watcher

--- 05-minivue/js/compiler.js
+++ 05-minivue/js/compiler.js
@@ -41,15 +41,23 @@ class Compiler {
   }
   update(node, key, attrName) {
     const updateFn = this[attrName + 'Updater']
-    updateFn && updateFn(node, this.vm[key])
+    updateFn && updateFn.call(this, node, this.vm[key], key)
   }
   // 处理 v-text 指令
-  textUpdater(node, value) {
+  textUpdater(node, value, key) {
     node.textContent = value
+
+    new Watcher(this.vm, key, (newVal) => {
+      node.textContent = newVal
+    })
   }
   // 处理 v-mode
-  modelUpdater(node, value) {
+  modelUpdater(node, value, key) {
     node.value = value
+
+    new Watcher(this.vm, key, (newVal) => {
+      node.value = newVal
+    })
   }
   // 编译文本节点，处理插值表达式
   compileText(node) {
@@ -58,6 +66,11 @@ class Compiler {
     if (reg.test(value)) {
       const key = RegExp.$1.trim()
       node.textContent = value.replace(reg, this.vm[key])
+
+      // 创建 watcher 对象，当数据改变需要更新视图
+      new Watcher(this.vm, key, (newVal) => {
+        node.textContent = newVal
+      })
     }
   }
   // 判断元素属性是否是指令
