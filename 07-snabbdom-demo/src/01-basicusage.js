860f5a6a7c22ec313697c24166e52ba3b302f5ee
aicherish <aicherish0320@163.com>
2022-03-13 17:41:16 +0800

    createElm 方法调试

--- 07-snabbdom-demo/src/01-basicusage.js
+++ 07-snabbdom-demo/src/01-basicusage.js
@@ -2,7 +2,20 @@ import { h, init } from 'snabbdom'
 
 const patch = init([])
 // 第一个参数：标签+选择器；第二个参数：如果是字符串就是标签中的文本内容
-let vnode = h('div#container.cls', 'hello world')
+let vnode = h(
+  'div#container.cls',
+  {
+    hook: {
+      init(vnode) {
+        console.log(vnode.elm)
+      },
+      create(emptyNode, vnode) {
+        console.log(vnode.elm)
+      }
+    }
+  },
+  'hello world'
+)
 const app = document.querySelector('#app')
 // 第一个参数：旧的VNode，可以是 dom 元素
 // 第二个参数：新的VNode
